5f3edb0c2c2a5ab9b4390f3d57f849f6d1ecf255
Марат <D:\Program Files\The Bat!\Mail>
2022-05-14 15:04:41 +0600

    useCallback

--- src/app/components/examples/useCallback/useCallBackExample.jsx
+++ src/app/components/examples/useCallback/useCallBackExample.jsx
@@ -1,11 +1,54 @@
-import React from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import CardWrapper from "../../common/Card";
 import SmallTitle from "../../common/typografy/smallTitle";
 
 const UseCallBackExample = () => {
+    const [data, setData] = useState({});
+    const withOutCallback = useRef(0);
+    const withCallback = useRef(0);
+
+    const handleChange = ({ target }) => {
+        setData((prevState) => ({ ...prevState, [target.name]: target.value }));
+    };
+
+    // WithOutCallback
+    const validateWithOutCallback = (data) => {
+        console.log(data);
+    };
+    useEffect(() => {
+        withOutCallback.current++;
+    }, [validateWithOutCallback]);
+
+    // WithCallback
+    const validateWithCallback = useCallback((data) => {
+        console.log(data);
+    }, []);
+    useEffect(() => {
+        withCallback.current++;
+    }, [validateWithCallback]);
+
+    // При изменение data запускаем функции
+    useEffect(() => {
+        validateWithOutCallback(data);
+        validateWithCallback(data);
+    }, [data]);
+
     return (
         <CardWrapper>
             <SmallTitle>Example</SmallTitle>
+            <p>Render withOutCallback : {withOutCallback.current}</p>
+            <p>Render withCallback : {withCallback.current}</p>
+            <label htmlFor="email" className="form-label">
+                Email
+            </label>
+            <input
+                type="email"
+                className="form-control mb-2"
+                id="email"
+                name="email"
+                value={data.email || ""}
+                onChange={handleChange}
+            />
         </CardWrapper>
     );
 };
